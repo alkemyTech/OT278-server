dc58c3507a8dfbc63365355ea0d3538051edd344
Lucas López <lucaslopezd@hotmail.com>
2022-09-03 00:48:40 -0300

    New email after save a new contact / emailService refactored

--- src/main/java/com/alkemy/ong/controller/ContactController.java
+++ src/main/java/com/alkemy/ong/controller/ContactController.java
@@ -2,6 +2,8 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.ContactDto;
 import com.alkemy.ong.service.IContactService;
+import com.alkemy.ong.service.IEmailService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +20,12 @@ import javax.validation.Valid;
 public class ContactController {
 
     private final IContactService service;
+    private final IEmailService emailService;
 
     @PostMapping
     public ResponseEntity<ContactDto> save(@Valid @RequestBody ContactDto contact) {
         ContactDto savedContact = service.save(contact);
+        emailService.sendEmail(savedContact.getEmail());
         return ResponseEntity.status(HttpStatus.CREATED).body(savedContact);
     }
 
