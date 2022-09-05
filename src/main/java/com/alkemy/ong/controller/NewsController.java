f59e869d2104773bf9a0993d5d8731213807e4d6
Gian Barritta <gianbdeveloper@hotmail.com>
2022-09-05 16:10:58 -0300

    Simplifying code

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -3,6 +3,7 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.response.NewsResponseDto;
 import com.alkemy.ong.service.INewsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +19,8 @@ public class NewsController {
 
     @GetMapping("/{id}")
     public ResponseEntity<NewsResponseDto> getById(@PathVariable Long id) {
-        NewsResponseDto news = service.getById(id);
-        return ResponseEntity.ok(news);
+
+        return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
     }
 
 }
