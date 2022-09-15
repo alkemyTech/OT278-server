d514432e8617ee661a69157c2b772215f531c455
Lucas López <lucaslopezd@hotmail.com>
2022-09-14 21:00:32 -0300

    triying to resolve inssues of examples errors

--- src/main/java/com/alkemy/ong/exception/CustomExceptionDetails.java
+++ src/main/java/com/alkemy/ong/exception/CustomExceptionDetails.java
@@ -1,5 +1,7 @@
 package com.alkemy.ong.exception;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -7,10 +9,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @RequiredArgsConstructor
+@ApiModel(value = "Model Error Response")
 public class CustomExceptionDetails {
 
+    @ApiModelProperty(value = "Error/Exception name", example = "ErrorException", position = 0)
     private String exception;
+    @ApiModelProperty(value = "Details of message", example = "An Error ocurred when...", position = 1)
     private String message;
+    @ApiModelProperty(value = "Path where the error become", example = "resouce/service/method", position = 2)
     private String path;
 
     public CustomExceptionDetails(Exception e, String path){
