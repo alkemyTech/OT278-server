934e22fa6c8794828bb0bbfd5866c20cc0360ab6
Oscar Talero <talero@gmail.com>
2022-09-15 07:23:42 -0500

    Add pagination to Testimonials

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -2,8 +2,13 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.testimonial.TestimonialRequestDto;
 import com.alkemy.ong.dto.testimonial.TestimonialResponseDto;
+import com.alkemy.ong.model.Testimonial;
+import com.alkemy.ong.repository.TestimonialRepository;
 import com.alkemy.ong.service.ITestimonialService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +21,7 @@ import javax.validation.Valid;
 public class TestimonialController {
 
     private final ITestimonialService service;
+    private final TestimonialRepository testimonialRepository;
 
     @PostMapping
     public ResponseEntity<TestimonialResponseDto> save(@Valid @RequestBody TestimonialRequestDto testimonial) {
@@ -35,4 +41,8 @@ public class TestimonialController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @GetMapping
+    public Page<Testimonial> getAll(@PageableDefault(page=0, size = 10)Pageable pageable) {
+        return testimonialRepository.findAll(pageable);
+    }
 }
